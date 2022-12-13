0a59d713d19831256c1d7d40434385e78bcc986b
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 17:50:44 +0800

    Creating comments

--- PriorityHeap.py
+++ PriorityHeap.py
@@ -1,3 +1,9 @@
+# This program is for Representing Priority Queues with a Heap.
+
+
+# The point of a heap isn’t so much about sorting elements but rather keeping them in a
+# certain relationship to allow for quick lookup.
+
 from heapq import heappush
 
 fruits = []
@@ -7,4 +13,6 @@ heappush(fruits, "Banana")
 heappush(fruits, "Mango")
 heappush(fruits, "Grapes")
 
+
+# Fruit names in the resulting heap in the example don’t follow alphabetical order.
 print("These are the elements being sorted:\n", fruits)
