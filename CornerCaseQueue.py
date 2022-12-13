e50766a3c5a1ce749b72c6aecac9df2102f11c30
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 23:58:34 +0800

    FINAL CODE FOR CORNER CASE PRIORITY QUEUE

--- CornerCaseQueue.py
+++ CornerCaseQueue.py
@@ -18,6 +18,7 @@ class Message:
 wipers = Message("Windshield wipers turned on")
 hazard_lights = Message("Hazard lights turned on")
 
+#Comparison Operators
 wipers < hazard_lights
 
 messages = PriorityQueue()
