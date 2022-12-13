ba81df3b58cc99e0350512919277b0f86152e7c7
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 21:51:22 +0800

    Changing Lines of Codes

--- PriorityQueueNegative.py
+++ PriorityQueueNegative.py
@@ -1,4 +1,13 @@
 
 from queues4 import PriorityQueue
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
 messages = PriorityQueue()
-print(messages.dequeue())
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
