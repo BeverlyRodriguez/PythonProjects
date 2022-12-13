5a960b0a28162f9ebffd859f9748c660c9b79e9d
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 19:37:28 +0800

    Adding other Printing Statements

--- PriorityTuple.py
+++ PriorityTuple.py
@@ -5,3 +5,5 @@ person2 = ("Beverly", "Doe", 30)
 person3 = ("Beverly", "Doe", 20)
 
 print("PERSON 1 < PERSON 3 is:  ", person1 < person2)
+print("PERSON 2 < PERSON 3 is:  ", person2 < person3)
+print("PERSON 1 < PERSON 2 is:  ", person1 < person2)
