7362af390c5244f53039eefaedfbe50f4c660ad7
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 21:13:45 +0800

    Adding Comments

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -1,6 +1,6 @@
 from queues3 import PriorityQueue
 
-
+# priority levels: critical, important, and neutral.
 CRITICAL = 3
 IMPORTANT = 2
 NEUTRAL = 1
@@ -11,5 +11,5 @@ messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
 messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
 messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
 
-
+#Message Deque with the lowest priority. 
 print(messages.dequeue())
