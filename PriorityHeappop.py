145d5194ddc18ba655f6ee3d0b3a5951ef579e60
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 18:22:29 +0800

    Heappop Fruits

--- PriorityHeappop.py
+++ PriorityHeappop.py
@@ -1,2 +1,11 @@
 from heapq import heappop
 
+
+fruits = []
+heappop(fruits, "Orange")
+heappop(fruits, "Apple")
+heappop(fruits, "Banana")
+heappop(fruits, "Mango")
+heappop(fruits, "Grapes")
+
+heappop(fruits)
