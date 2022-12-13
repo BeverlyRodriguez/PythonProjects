a55edb80470e12e4167a2b8715aff3d30cfbef24
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 19:02:47 +0800

    Adding Commenta

--- PriorityHeappop.py
+++ PriorityHeappop.py
@@ -1,11 +1,20 @@
 from heapq import heappush
 from heapq import heappop
 
-fruits = []
-heappush(fruits, "Orange")
-heappush(fruits, "Apple")
-heappush(fruits, "Banana")
-heappush(fruits, "Mango")
-heappush(fruits, "Grapes")
 
-print(heappop(fruits))
+# The first element on a heap always has the smallest (min-heap) or the highest (max-heap) 
+# value, depending on how you define the condition for the mentioned relationship. 
+vegetables = []
+heappush(vegetables, "Squash")
+heappush(vegetables, "Letuce")
+heappush(vegetables, "Eggplant")
+heappush(vegetables, "Cucumber")
+heappush(vegetables, "Celery")
+
+
+# When you pop an element from a heap, you’ll always get the first one, 
+# while the remaining elements might shuffle a little bit
+print(heappop(vegetables))
+print("These are the remaining elements: \n\t",vegetables)
+
+
