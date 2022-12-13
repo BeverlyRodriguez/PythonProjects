7d452afd96bfb3e36c70996366c792e824ec70fd
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 19:43:29 +0800

    Adding Variables

--- PriorityTuple.py
+++ PriorityTuple.py
@@ -3,8 +3,12 @@
 person1 = ("Beverly", "Brown", 55)
 person2 = ("Beverly", "Doe", 30)
 person3 = ("Beverly", "Doe", 20)
+person4 = ("Beverly", "Castro", 45)
+person5 = ("Beverly", "Zamora", 50)
 
 print("\nThese are the results: \n")
 print("\tPERSON 1 < PERSON 3 is:  ", person1 < person2)
 print("\tPERSON 2 < PERSON 3 is:  ", person2 < person3)
-print("\tPERSON 1 < PERSON 2 is:  ", person1 < person2)
+print("\tPERSON 1 < PERSON 4 is:  ", person1 < person4)
+print("\tPERSON 3 < PERSON 4 is:  ", person3 < person4)
+print("\tPERSON 2 < PERSON 5 is:  ", person2 < person5)
