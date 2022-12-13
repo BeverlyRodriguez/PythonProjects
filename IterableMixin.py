cf4538f1eed76139e0a3455eabdd53d0f11580cd
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-14 01:33:37 +0800

    Queue Iterable Mixin Class

--- IterableMixin.py
+++ IterableMixin.py
@@ -5,3 +5,21 @@ class IterableMixin:
     def __iter__(self):
         while len(self) > 0:
             yield self.dequeue()
+
+class Queue(IterableMixin):
+    class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
