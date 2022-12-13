cab2bf501e215f4c38063b5a8cc5f1d55ec198ac
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 19:45:01 +0800

    Adding Comments

--- PriorityTuple.py
+++ PriorityTuple.py
@@ -1,4 +1,6 @@
 
+# Python’s tuple comparison, which takes into account the tuple’s 
+# components, looking from left to right until the outcome is known.
 
 person1 = ("Beverly", "Brown", 55)
 person2 = ("Beverly", "Doe", 30)
@@ -6,6 +8,7 @@ person3 = ("Beverly", "Doe", 20)
 person4 = ("Beverly", "Castro", 45)
 person5 = ("Beverly", "Zamora", 50)
 
+#These are the printing statements.
 print("\nThese are the results: \n")
 print("\tPERSON 1 < PERSON 3 is:  ", person1 < person2)
 print("\tPERSON 2 < PERSON 3 is:  ", person2 < person3)
