e74e3d386a5bf7193b635419e63e1ba2f97ab93f
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-13 20:03:29 +0800

    FINAL CODE FOR PRIORITY TUPLE COMPARISON

--- PriorityTuple.py
+++ PriorityTuple.py
@@ -12,7 +12,7 @@ person3 = ("Beverly", "Doe", 20)
 person4 = ("Beverly", "Castro", 45)
 person5 = ("Beverly", "Zamora", 50)
 
-#These are the printing statements.
+# These are the printing statements.
 print("\nThese are the results: \n")
 print("\tPERSON 1 < PERSON 3 is:  ", person1 < person2)
 print("\tPERSON 2 < PERSON 3 is:  ", person2 < person3)
