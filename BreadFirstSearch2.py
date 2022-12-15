9ed7d43a229b884b5dd51074a123e407ce96daf5
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-15 17:11:48 +0800

    FINAL CODE FOR BREAD FIRST SEARCH

--- BreadFirstSearch2.py
+++ BreadFirstSearch2.py
@@ -2,4 +2,26 @@
 # Sorting the neighbors according to some criteria.
 
 
+import networkx as nx
+from grapmain2 import City, load_graph
+
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
+
+for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
+
 
