23123b67364d570d3703149f863516f326bb20d4
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-15 15:59:26 +0800

    FINAL CODE FOR GRAPH NODES

--- graphinitial.py
+++ graphinitial.py
@@ -1,8 +1,11 @@
 # This program shows the Object Representation of the Cities and Roads.
+# Using 3.9 Python Interpreter.
 
 print("\n\t\t********PROGRAMMED BY:********")
 print("\t\t***BEVERLY ANN L. RODRIGUEZ***\n")
 
+# Networkx represents graph nodes using textual identifiers 
+# that can optionally have an associated dictionary of attributes
 import networkx as nx
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 print("\nHere is the map information: \n")
