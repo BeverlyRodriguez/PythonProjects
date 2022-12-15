2ec28d5eac394a4f603cf1b39144472ab2effe45
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-15 13:03:20 +0800

    START

--- graphrun2.py
+++ graphrun2.py
@@ -1,15 +1,1 @@
-# The immediate neighbors of a given city.
-# Object Reprepsentation of the Cities and Roads. 
-
-
-print("\n\t\t********PROGRAMMED BY:********")
-print("\t\t***BEVERLY ANN L. RODRIGUEZ***\n")
-
-
-from grapmain2 import City, load_graph
-
-nodes, graph = load_graph("roadmap.dot", City.from_dict)
-print("These are the neighbors of the LONDON: \n")
-for neighbor, weights in graph[nodes["london"]].items():
-
-    print(weights["distance"], neighbor.name)
+#START
