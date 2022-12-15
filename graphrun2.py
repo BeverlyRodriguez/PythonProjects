3e1c385f5409a780726ea466fdad8f50d0af4028
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-15 14:58:03 +0800

    FINAL CODE FOR NEIGHBORS

--- graphrun2.py
+++ graphrun2.py
@@ -2,18 +2,19 @@
 # Graph for neighboring cities.
 
 
-# This is for neighbor cities with no distance.
+
 print("\n\t\t********PROGRAMMED BY:********")
 print("\t\t***BEVERLY ANN L. RODRIGUEZ***\n")
 
 from grapmain2 import City, load_graph
 
-#This is for neighbor cities with distance.
+# This is for neighbor cities with no distance.
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 print("\n\nNeighbors Cities: \n")
 for neighbor in graph.neighbors(nodes["london"]):
     print(neighbor.name)
 
+#This is for neighbor cities with distance.
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 print("\n\nNeighbors Cities with Distance: \n")
 for neighbor, weights in graph[nodes["london"]].items():
