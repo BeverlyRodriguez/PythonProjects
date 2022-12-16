49a60c7d224e07fa479997959513eacf803a55f1
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-16 20:13:59 +0800

    FINAL CODE FOR DEPTH-FIRST SEARCH USING A LIFO QUEUE

--- DepthFirstSearch.py
+++ DepthFirstSearch.py
@@ -3,6 +3,9 @@
 # plunging into the graph as deeply as possible before backtrackinG
 # to the last edge crossing and trying another branch.
 
+print("\n\t\t********PROGRAMMED BY:********")
+print("\t\t***BEVERLY ANN L. RODRIGUEZ***\n")
+
 import networkx as nx
 from graphmain3 import City, load_graph
 
