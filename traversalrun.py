d71932ce8dd7a90c8e8908839cbbcccf16080bdc
Beverly Ann L. Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-12-16 20:12:47 +0800

    FINAL CODE FOR SHORTEST PATH USING BREADTH FIRST TRAVERSAL

--- traversalrun.py
+++ traversalrun.py
@@ -1,5 +1,7 @@
 # Shortest Path Using Breadth-First Traversal.
-
+# Using networkx to reveal all the shortest paths between two cities.
+# Traversing the graph using the breadth-first approach will produce a path guaranteed to have the fewest nodes. 
+# There are two such shortest paths between Aberdeen and Perth when you disregard the road distances. 
 
 print("\n\t\t********PROGRAMMED BY:********")
 print("\t\t***BEVERLY ANN L. RODRIGUEZ***\n")
